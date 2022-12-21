323d7ccbd56cfd55ab464d01eb9e02abc232edac
Ryan <86922669+AuraCodez@users.noreply.github.com>
2022-12-20 21:56:33 -0500

    Add files via upload

---
+++ GroceryStore.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+public class GroceryStore implements Comparable<Item>  {
+	private ArrayList<Item> items;
+	
+	public static void main(String [] args) {
+		GroceryStore store = new GroceryStore();
+		store.addItem(new Item("Grape", 4, 10));
+		store.addItem(new Item("Orange", 5, 5));
+		store.addItem(new Item("Pear", 5, 5));
+		store.addItem(new Item("Apple", 4, 10));
+		store.displayItems();
+		System.out.println(store.getTotalPriceOfGoods());
+		System.out.println(store.search("Grape"));
+		System.out.println(store.getName());
+	}
+	
+	public GroceryStore() {
+		this.items = new ArrayList<Item>();
+	}
+	
+	public void addItem(Item obj) {
+		this.items.add(obj);
+		Collections.sort(this.items);
+		
+	}
+	
+	public String getName() { //Get name of Item
+		String name = "";
+	     for(int i = 0; i < this.items.size(); i++) {
+	    	 Item a = this.items.get(i);
+	    	 name += a.getName();
+	     }
+	     return name;
+	     
+	}
+	
+	public void setNewStock(String name, int newStock) {
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			if(a.getName().equals(name)) {
+				a.setStock(newStock);
+				break;
+			}
+		}
+	}
+	
+	public double getTotalPriceOfGoods() { //This methods get the total price of the items
+		double price = 0;
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			price += a.getTotalPrice();
+		}
+		return price;
+		
+	}
+	
+	public void removeItem(String item) {
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			if(a.getName().equals(item)) {
+				this.items.remove(i);
+				i--; //Decrement value i after removing an element.
+			}
+		}
+		
+	}
+	
+	public void increaseStock(String name, int quantity) {
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			if(a.getName().equals(name)) {
+			   if(quantity > 0) {
+				   a.increaseStock(quantity);
+				   break;
+			   }
+			}
+		}
+	}
+	
+	public void decreaseStock(String name, int quantity) {
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			if(a.getName().equals(name)) {
+				if(quantity != 0) {
+					a.decreaseStock(quantity);
+					break;
+				}
+			}
+		}
+	}
+	
+	public void displayItems() { 
+		for(int i = 0; i < this.items.size(); i++) {
+			Item a = this.items.get(i);
+			System.out.println("Item: " + a.getName() + "  Price: " + a.getPrice() + " Current Stock: "+ a.getStock());
+		}
+	}
+	
+
+
+	public int search(String name) {
+		int low = 0;
+		int high = this.items.size() - 1;
+		while(low <= high) {
+			int mid = (low + high)/ 2;
+			Item midItem = this.items.get(mid);
+			if(name.equals(midItem.getName())) {
+				return mid;
+			} else if(name.compareTo(midItem.getName()) < 0) {
+				low = mid + 1;
+				
+			} else if(name.compareTo(midItem.getName()) > 0) {
+				high = mid - 1;
+			}
+			
+		}
+		return -1; //Item is not found in items
+		
+	}
+
+	@Override
+	public int compareTo(Item o) {
+		return this.getName().compareTo(o.getName());
+	}
+
+}
---
+++ Item.java
@@ -0,0 +1,88 @@
+
+public class Item implements Comparable<Item> {
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public static void main(String [] args) {
+		Item item = new Item("Apple", 0.99, 10);
+		System.out.println(item.inStock());
+		Item idk = new Item("Chips", 0.54, 32);
+		System.out.println(idk.getTotalPrice());
+		System.out.println(idk.toString());
+	}
+	
+	public Item(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void increasePrice(int amount) {
+		this.price += amount;
+	}
+	
+	public void decreasePrice(int amount) {
+		this.price -= amount;
+	}
+	
+	public int getStock() {
+		return this.quantity;
+	}
+	
+	public void increaseStock(int amount) {
+		this.quantity += amount;
+	}
+	
+	public void decreaseStock(int amount) {
+		this.quantity -= amount;
+	}
+	
+	public void goneBad() {
+		this.quantity = 0;
+	}
+	
+	public boolean inStock() {
+		if(this.quantity > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean inStockName(String name) {
+	    return this.name.equals(name) && this.quantity > 0;
+	}
+	
+	public void setStock(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double getTotalPrice() {
+		double price = this.price * quantity;
+		return price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Item: " + this.getName() + " " + " Price: " + this.getPrice() + " " + " Stock: " + this.getStock();
+	}
+
+	@Override
+	public int compareTo(Item other) {
+		return this.name.compareTo(other.getName());
+	}
+}
